df2fddfb022fff1f7eb4b20760d27e8da7e3219b
CoreyKabbage <63615431+CoreyKabbage@users.noreply.github.com>
2020-11-20 22:01:52 -0700

    Added ship image
    Added ship.bmp and made it blit to the screen.

--- AlienInvasion/Scratchpad.py
+++ AlienInvasion/Scratchpad.py
@@ -3,12 +3,14 @@ import sys
 import pygame
 
 from Settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
-    #Overall class to manage game assets and behavior.
+    # Overall class to manage game assets and behavior.
 
     def __init__(self):
-        #Initialize the game and create game resources
+        # Initialize the game and create game resources
         pygame.init()
         self.settings = Settings()
 
@@ -17,21 +19,23 @@ class AlienInvasion:
 
         self.bg_color = self.settings.bg_color
 
+        self.ship = Ship(self)
+
     def run_game(self):
-        #Start the main loop for the game.
+        # Start the main loop for the game.
         while True:
-            #Watch for keyboard and mouse events.
+            # Watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            #Make the most recently drawn screen visible.
+            # Make the most recently drawn screen visible.
             pygame.display.flip()
 
             self.screen.fill(self.bg_color)
-
+            self.ship.blitme()
 
 if __name__ == '__main__':
-    #Make a game instance, and run the game.
+    # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
